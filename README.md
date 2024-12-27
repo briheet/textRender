309309e5783580e3b9af743ba9034636ac179350
briheet <briheetyadav@gmail.com>
2024-12-27 21:16:43 +0530

    displaying img

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Would be rendering text on a given image.
 Want to put it up on opening operating system.
 It looks boring while opening a sddm or lightdm.
 
-[]: Image check
-[]: GUI maybe
+[X]: Image check
+[X]: opencv pull and display window
+[]: render text
--- render.py
+++ render.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-print("Hi")
+import cv2 as cv
+import sys
+
+def start():
+    img = cv.imread(cv.samples.findFile("wallhaven-kxy7j6.jpg"))
+    if img is not None:
+        img = cv.resize(img, (1000, 540))
+
+    if img is None:
+        sys.exit("No such image")
+
+    cv.imshow("Display window", img)
+
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+def main():
+    start() # This continues to work until the window is closed
+    print("HI")
+
+
+if __name__ == "__main__":
+    main()
