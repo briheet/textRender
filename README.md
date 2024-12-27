5fe2a92b97d68a808439e00e9bd3e29345466527
briheet <briheetyadav@gmail.com>
2024-12-28 00:19:08 +0530

    showing text, double press esc for escape

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Would be rendering text on a given image.
 Want to put it up on opening operating system.
 It looks boring while opening a sddm or lightdm.
 
-[X]: Image check
-[X]: opencv pull and display window
-[]: render text
+[X]: Image check.\
+[X]: Opencv pull and display window.\
+[X]: Render text.\
+[]: Change text on runtime.
--- render.py
+++ render.py
@@ -3,24 +3,38 @@
 import cv2 as cv
 import sys
 
+sample = "wallhaven-kxy7j6.jpg"
+font = cv.FONT_HERSHEY_SIMPLEX
+distance = 10
+
 def start():
-    img = cv.imread(cv.samples.findFile("wallhaven-kxy7j6.jpg"))
+    img = cv.imread(cv.samples.findFile(sample))
     if img is not None:
         img = cv.resize(img, (1000, 540))
 
     if img is None:
         sys.exit("No such image")
 
-    cv.imshow("Display window", img)
+    i = 10
+    while True:
+        cv.imshow("Display", img)
+
+        k = cv.waitKey(33)
+        if k == 27:
+            break
+        elif k == -1:
+            continue
+        else:
+            print(k)
+            cv.putText(img, 'Distance: {}'.format(distance), (i, 50), font, 3, (0, 255, 0), 2, cv.LINE_AA)
 
     cv.waitKey(0)
-
     cv.destroyAllWindows()
 
 
 def main():
     start() # This continues to work until the window is closed
-    print("HI")
+    print("Hi")
 
 
 if __name__ == "__main__":
